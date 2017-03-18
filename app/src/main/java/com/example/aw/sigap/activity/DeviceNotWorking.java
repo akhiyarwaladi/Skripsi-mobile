b4aa42f8f99a08937aa9f6b382a5a918ebbf58a6
akhiyarwaladi <akiyar18@gmail.com>
2017-03-19 05:46:45 +0700

    activity notification

---
+++ app/src/main/java/com/example/aw/sigap/activity/DeviceNotWorking.java
@@ -0,0 +1,15 @@
+package com.example.aw.sigap.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.aw.sigap.R;
+
+public class DeviceNotWorking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device_not_working);
+    }
+}
